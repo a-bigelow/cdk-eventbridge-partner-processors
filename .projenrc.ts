0ee14b15881f8616fc3f89ee85b5e973814ba889
a-bigelow <adam@adambigelow.com>
2022-08-13 21:50:48 -0400

    chore(Go): changing Golang release target

--- .projenrc.ts
+++ .projenrc.ts
@@ -15,7 +15,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     module: 'a-bigelow.cdk-eventbridge-partner-processors',
   },
   publishToGo: {
-    moduleName: 'github.com/a-bigelow/cdk-eventbridge-partner-processors',
+    moduleName: 'github.com/a-bigelow/cdk-eventbridge-partner-processors-go',
   },
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
